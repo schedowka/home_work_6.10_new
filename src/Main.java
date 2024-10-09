6c8581fef6cc6b54e758c261916f3ba90d3a2a81
schedowka <marina_molodkina@proton.me>
2024-10-09 18:17:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int days = 234;
+        int week = days/7;
+        System.out.println(week);
+
+         }
+}
